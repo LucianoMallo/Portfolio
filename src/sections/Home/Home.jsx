712fa7537d7fab565e330da15d9f759ac16517df
Luciano Mallo <malloluciano@gmail.com>
2023-06-13 15:41:09 +0200

    eslint corrections

--- src/sections/Home/Home.jsx
+++ src/sections/Home/Home.jsx
@@ -10,13 +10,13 @@ class Home extends React.Component {
         <h2 className='Home-description'>
           Hello! I'm Luciano.
         </h2>
-        <a>
+        <p>
           This is my personal page. Here you can find information about me, my projects, and my contact information.
           Also, you can find my resume in the following link:
           <Link to='/about' className='About'>
             About Me
           </Link>
-        </a>
+        </p>
       </div>
     )
   }
